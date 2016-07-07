ea4a892b562a72b58e97ae8726ae4ab1e460c965
uraway <masato.u0107@gmail.com>
2016-07-07 23:56:43 +0900

    refactor prepare

---
+++ scripts/prepare.js
@@ -0,0 +1,19 @@
+import fs from 'fs-extra';
+import pkg from '../package.json';
+
+const KEYS = ['author', 'dependencies', 'description', 'main', 'name', 'version'];
+const DATA = KEYS.reduce((object, key) => {
+  return Object.assign(object, { [key]: pkg[key] });
+}, {});
+
+try {
+  fs.mkdirSync('./app');
+} catch (error) {
+  if (error.code !== 'EEXIST') {
+    throw error;
+  }
+}
+
+fs.copySync('./src/assets', './app/assets');
+fs.writeFileSync('./app/index.html', fs.readFileSync('./src/index.html'));
+fs.writeFileSync('./app/package.json', JSON.stringify(DATA));
