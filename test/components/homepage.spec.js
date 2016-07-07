a0bfcca3ae26e2c8fd3c42bf8d5660f2edb1c597
uraway <masato.u0107@gmail.com>
2016-07-07 19:48:55 +0900

    remove tests

--- test/components/homepage.spec.js
+++
@@ -1,9 +0,0 @@
-import HomepageComponent from "components/homepage";
-
-describe("HomepageComponent", () => {
-  const element = shallow(<HomepageComponent />);
-
-  it("renders as a <div> element", () => {
-    expect(element.type()).to.eq("div");
-  });
-});
