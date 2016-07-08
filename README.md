c9553a1e1d37534190ac19489afac8c6d5d37dde
uraway <masato.u0107@gmail.com>
2016-07-08 15:08:21 +0900

    release

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 WIP...
+
+## Github Release
+
+Consider letting only Travis CI to upload your distributions to Github Release.
+
+#### local
+Set enviromental variable `GH_TOKEN`. Check your [personal access token](https://github.com/settings/tokens).
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { ipcRenderer } from 'electron';
 import version from '../../config/version';
 
 const Home = () => {
+  const checkForUpdate = () => {
+    ipcRenderer.send('check-update');
+  };
   const styles = {
     content: {
       margin: '32px',
@@ -11,7 +15,10 @@ const Home = () => {
   return (
     <div style={styles.content}>
       <h1>Welcome to React Redux Electron Builder Boilerplate</h1>
-      <p>VERSION {version}</p>
+      <p>CURRENT VERSION {version}</p>
+      <button onClick={checkForUpdate}>
+        Check for Update
+      </button>
     </div>
   );
 };
